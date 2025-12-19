f5bfd566c76f3c96085679c13e71583c0844f0db
Shrithik Shahapure <shrithik.shahapure12@gmail.com>
2025-12-19 10:16:47 -0500

    Able to Sample from the model.

--- train-model.py
+++ train-model.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import math
+import os
 
 # -----------------------------------------------------------------------------------------------------------------
 
@@ -177,6 +178,101 @@ class GPT(nn.Module):
 
         return model
             
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+# import tiktoken
+# import numpy as np
+
+# def load_tokens(filename):
+#     npt = np.load(filename)
+#     npt = np.astype(np.int32)
+#     ptt = torch.tensor(npt,dtype=torch.long)
+#     return ptt
+
+# class DataLoaderLite:
+#     def __init__(self,B,T,process_rank,num_processes,split):
+#         self.B = B
+#         self.T = T
+#         self.process_rank = process_rank
+#         self.num_processes = num_processes
+#         assert split in {'train', 'val'}
+
+
+
+#         data_root = "edu_fineweb10B"
+#         shards = os.listdir(data_root)
+#         shards = sorted(shards)
+#         shards = [os.path.join(data_root,s) for s in shards]
+#         self.shards = shards
+#         assert len(shards) > 0, f"No shards in split {split}"
+#         self.reset()
+
+#     def reset(self):
+
+#         self.current_shard = 0
+#         self.tokens = load_tokens(self.shards[self.current_shard])
+#         self.current_position = self.B * self.T * self.process_rank
+
+    
+#     def next_batch(self):
+
+#         B,T = self.B, self.T
+#         buf = self.tokens[self.current_position:self.current_position+B*T+1]
+#         x = (buf[:-1]).view(B,T)
+#         y = (buf[1:]).view(B,T)
+
+#         self.current_position += B * T * self.num_processes
+
+#         if self.current_position+ (B * T * self.num_processes + 1) > len(self.tokens):
+#             self.current_shard = (self.current_shard + 1) % len(self.shards)
+#             self.tokens = load_tokens(self.shards[self.current_shard])
+#             self.current_position = B * T * self.process_rank
+
+#         return x, y
+
+
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+num_return_sequences = 5
+max_length = 30
+device = 'cpu'
 
 model = GPT.from_pretrained('gpt2')
-print("Did not crash lmao")
+model.eval()
+# model.to('cuda') # need to get a gpu cluster
+
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I am Language Model")
+tokens = torch.tensor(tokens, dtype = torch.long)
+tokens = tokens.unsqueeze(0).repeat( num_return_sequences, 1) 
+
+
+torch.manual_seed(42)
+
+
+while tokens.size(1) < max_length:
+    with torch.no_grad():
+        logits , _ = model(tokens)
+        logits = logits[:,-1,:]
+        probs = F.softmax(logits,dim = -1)
+        topk_probs,topk_indices = torch.topk( probs , 50 ,dim = -1) 
+        ix = torch.multinomial(topk_probs,1)
+        xcol = torch.gather(topk_indices,-1,ix)
+        tokens = torch.cat((tokens,xcol),dim = 1)
+
+
+for i in range(num_return_sequences):
+    row = tokens[i, :max_length].tolist()
+    decoded = enc.decode(row)
+    print(">", decoded)
+
+# for i in range(num_return_sequences):
+#     tokens = tokens[i,:max_length].tolist()
+#     decoded = enc.decode(tokens)
+#     print(">",decoded)
+
